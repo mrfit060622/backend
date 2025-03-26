bfd1fb3744313ef5dc76d7f1bf492f6551ffb2f1
Paulo Matos <mrpmatos@outlook.com>
2025-03-26 08:08:33 -0300

    Backend finalmente atualizado

--- run.py
+++ run.py
@@ -10,4 +10,6 @@ if __name__ == '__main__':
     app = create_app()
     with app.app_context():
         init_db()  # Inicializa o banco dentro do contexto da aplicação
-    app.run(host="0.0.0.0", port=5000, debug=True)
+
+    port = int(os.environ.get("PORT", 5000))
+     app.run(host="0.0.0.0", port=port, debug=False)
