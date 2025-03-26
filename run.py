759da5b59f9f076e2d2cc5b7b06db1c7790b551e
Paulo Matos <mrpmatos@outlook.com>
2025-03-26 08:36:58 -0300

    Backend finalmente agora vai

--- run.py
+++ run.py
@@ -6,10 +6,14 @@ from mrfit_app.db_setup import init_db  # Ajustado caminho da importação
 # Carrega variáveis de ambiente do .env
 load_dotenv()
 
-if __name__ == '__main__':
-    app = create_app()
-    with app.app_context():
-        init_db()  # Inicializa o banco dentro do contexto da aplicação
+# Criação da aplicação
+app = create_app()
 
+# Inicializa o banco de dados apenas uma vez ao iniciar
+with app.app_context():
+    init_db()
+
+# Verifica se estamos executando diretamente ou se o ambiente é de produção
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
