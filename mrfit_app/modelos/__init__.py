5131488ec87ea8a91245a0c39771d0b88495f7b2
Paulo Matos <mrpmatos@outlook.com>
2025-06-05 22:29:53 -0300

    corrigindo back

--- mrfit_app/modelos/__init__.py
+++ mrfit_app/modelos/__init__.py
@@ -1,3 +1,7 @@
+from mrfit_app.modelos.usuario import Usuario
+from mrfit_app.modelos.calorias import Caloria
+from mrfit_app.modelos.objetivo import Objetivo
+from mrfit_app.modelos.atividade import Atividade
 from mrfit_app.modelos.pagamento import Pagamento
 from mrfit_app.modelos.relatorio import Relatorio
 from mrfit_app.modelos.relatorio_gratis import RelatorioGratuito
