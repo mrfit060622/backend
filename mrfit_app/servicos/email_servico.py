6829520dbb94d2d72c7c4677d7cee1bbcccdcdec
mrfit060622 <mrpmatos@outlook.com>
2025-10-12 23:32:04 -0300

    Update email_servico.py

--- mrfit_app/servicos/email_servico.py
+++ mrfit_app/servicos/email_servico.py
@@ -3,7 +3,7 @@ from flask_mail import Message
 from urllib.parse import urlparse
 from flask import current_app
 
-def enviar_email(mail, email: str, pdf_filename: str, codigo: str) -> str:
+def enviar_email(mail, email: str, codigo: str,pdf_filename: str = None ) -> str:
     """Envia um e-mail com o link para download do PDF."""
     
     api_host = current_app.config['API_HOST']
