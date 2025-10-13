6c4d0e01d0e1cac0d3af6841a3b21cf34e861378
mrfit060622 <mrpmatos@outlook.com>
2025-10-13 00:38:54 -0300

    Krl

--- mrfit_app/servicos/email_servico.py
+++ mrfit_app/servicos/email_servico.py
@@ -4,7 +4,7 @@ from flask import current_app
 import requests
 import os
 
-def enviar_email(mail,email: str, codigo: str,pdf_filename: str) -> str:
+def enviar_email(mail,email: str, pdf_filename: str, codigo: str) -> str:
     """Envia um e-mail com o link para download do PDF via API do Brevo."""
 
     api_host = current_app.config['API_HOST']
